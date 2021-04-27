4576ee90c81ea042a32215c93f865d40e501d592
jirka <jorgetimes2@gmail.com>
2021-04-27 11:52:54 +0200

    18.8 Retrying failed messages

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -50,6 +50,7 @@ class CommentCrudController extends AbstractCrudController
         yield ImageField::new('photoFilename')
             ->setBasePath('/uploads/photos')
             ->setLabel('Photo')
+            ->onlyOnIndex()
         ;
         yield TextField::new('state');
 
